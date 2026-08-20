f197bec13390757a735ccec0e23bb4a1f3961512
Francisco Miranda <chicodias@outlook.com>
2026-08-20 11:35:12 -0300

    Replace append with extend and fix instructor list

--- arrays.py
+++ arrays.py
@@ -35,8 +35,12 @@ tom = {
     'title': 'instructor'
 }
 
-contacts.append([tom, andrew, tony, matt, jake])
+contacts.extend([tom, andrew, tony, matt, jake])
 
 # 3. Woops after adding all of those people to the same contacts list you realized you need a list just the instructors.
 # create a new variable named instructors and populate it using the contacts list.
-instructors = contacts[contacts['title'] == 'instructor']
+instructors = []
+
+for contact in contacts:
+    if contact['title'] == 'instructor':
+        instructors.append(contact)
