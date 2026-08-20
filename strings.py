0f4e8952d30797ed5bfdc6c07b7fb1bde820b7f8
Francisco Miranda <chicodias@outlook.com>
2026-08-20 11:02:02 -0300

    Fix string manipulation and counting in strings.py

--- strings.py
+++ strings.py
@@ -2,14 +2,16 @@
 # Careful not to uppercase the i in the word pair
 
 sentence = 'When i went to the mall i bought a pair of shoes.'
-
+sentence.replace("i", "I")
+print(sentence)
 
 # 2. The history of Python is interesting but how many times does the letter a appear? Find a way to count both upper and lower case A's
 
 py_history = 'Python, not to be confused with the snake, was created by Guido van Rossum and first released in 1991. Python was named after Monty Python\'s Flying Circus, a British comedy group that Guido enjoyed. As a successor to the ABC language, Python was designed to be readable and easy to learn. In December of 1989 Guido began implementation as a hobby project over Christmas, and by February 1991 he published the first version. This was somewhat of a lasting move at the time, with Python later becoming very popular around data analysis and automation.'
-
+py_history.count('a') + py_history.count('A')
 
 # 3. Given the following sentence: Uppercase the entire word scream each time it appears in the sentence
 # I SCREAM, you SCREAM, we all SCREAM for icecream
 
 text = 'I scream, you scream, we all scream for icecream'
+text.replace("scream", "SCREAM")
